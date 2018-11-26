6f40ae533f8c90a89c79234b2f37a27c97310861
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2018-11-26 22:14:42 +0100

    updatesOnProjectNameChange
    Root name change

--- src/main/java/translate/ChatTranslateApplication.java
+++ src/main/java/translate/ChatTranslateApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Fall2018projectApplication {
+public class ChatTranslateApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Fall2018projectApplication.class, args);
+        SpringApplication.run(ChatTranslateApplication.class, args);
     }
 }
