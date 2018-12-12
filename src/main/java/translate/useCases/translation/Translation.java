7c19edd677951f677dcfcd2c0d96e283bb44eac3
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2018-12-12 19:19:30 +0100

    updateTranslationUseCase

--- src/main/java/translate/useCases/translation/Translation.java
+++ src/main/java/translate/useCases/translation/Translation.java
@@ -9,14 +9,38 @@ public class Translation {
 
     private static Map<String, String> translateToEnglish = new LinkedHashMap<>();
     private static Map<String, String> translateToGerman = new LinkedHashMap<>();
-    static {
-        translateToGerman.put("hello", "hallo");
-        translateToEnglish.put("hallo", "hello");
-        translateToGerman.put("how are you", "wie geht's");
-        translateToEnglish.put("wie geht's", "how are you");
-        translateToGerman.put("I'm fine. Thanks", "Ich bin gut. Danke");
-        translateToEnglish.put("Ich bin gut. Danke", "I'm fine. Thanks");
+    private static Map<String, String> translateSpanishToEnglish = new LinkedHashMap<>();
 
+    static {
+
+        // TRANSLATE TO GERMAN
+
+        // English to German
+        translateToGerman.put("Hello", "Hallo");
+        translateToGerman.put("How are you", "Wie geht's");
+        translateToGerman.put("I'm fine", "Ich bin gut");
+        translateToGerman.put("I'm fine. Thanks", "Ich bin gut. Danke");
+        translateToGerman.put("Thank you REDI", "Danke REDI");
+        translateToGerman.put("Have a nice evening", "Schonen Abend");
+        translateToGerman.put("Bye", "Tchuss");
+        translateToGerman.put("See you again", "Auf wiedersehen");
+        // English to Spanish
+
+
+
+
+
+        // TRANSLATE TO ENGLISH
+
+        // German to English
+        translateToEnglish.put("Hallo", "Hello");
+        translateToEnglish.put("Wie geht's", "How are you");
+        translateToEnglish.put("Ich bin gut", "I'm fine");
+        translateToEnglish.put("Ich bin gut. Danke", "I'm fine. Thanks");
+        translateToEnglish.put("Danke REDI", "Thank you REDI");
+        translateToEnglish.put("Schonen Abend", "Have a nice evening");
+        translateToEnglish.put("Tchuss", "Bye");
+//        translateToEnglish.put("")
     }
 
 
